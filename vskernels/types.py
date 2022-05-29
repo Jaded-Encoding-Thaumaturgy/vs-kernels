f607091b7f41ed60863c0c146725ed4af9fd4eab
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-05-29 19:48:03 +0200

    Add matrix typehinting

---
+++ vskernels/types.py
@@ -0,0 +1,26 @@
+from enum import IntEnum
+from typing import NoReturn
+
+
+class Matrix(IntEnum):
+    """Matrix coefficients (ITU-T H.265 Table E.5)"""
+    RGB = 0
+    GBR = 0
+    BT709 = 1
+    UNKNOWN = 2
+    _RESERVED = 3
+    FCC = 4
+    BT470BG = 5
+    SMPTE170M = 6
+    SMPTE240M = 7
+    YCGCO = 8
+    BT2020NC = 9
+    BT2020C = 10
+    SMPTE2085 = 11
+    CHROMA_DERIVED_NC = 12
+    CHROMA_DERIVED_C = 13
+    ICTCP = 14
+
+    @property
+    def RESERVED(self) -> NoReturn:
+        raise PermissionError
