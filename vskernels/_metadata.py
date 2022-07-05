e3bc4deec9d5e638ac61eb7d61f5cfed9b0a364d
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-07-06 00:25:17 +0200

    bump

--- vskernels/_metadata.py
+++ vskernels/_metadata.py
@@ -1,6 +1,6 @@
 """Kernel objects for scaling and format conversion within VapourSynth"""
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 __author__ = 'LightArrowsEXE <LightArrowsReboot@gmail.com>'
 __maintainer__ = __author__
