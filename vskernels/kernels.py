61ef045a1ef3a5ab0c44c779aec27b5bb8f576a8
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-24 13:45:20 +0200

    Move Example kernel at the bottom

--- vskernels/kernels.py
+++ vskernels/kernels.py
@@ -201,83 +201,6 @@ class Spline64(_DescalePlugin):
     descale_function = core.descale.Despline64
 
 
-class Example(Kernel):
-    """Example Kernel class for documentation purposes."""
-
-    def __init__(self, b: float = 0, c: float = 1/2, **kwargs: Any) -> None:
-        self.b = b
-        self.c = c
-        super().__init__(**kwargs)
-
-    def scale(self, clip: vs.VideoNode, width: int, height: int, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
-        """
-        Perform a regular scaling operation.
-
-        :param clip:        Input clip
-        :param width:       Output width
-        :param height:      Output height
-        :param shift:       Shift clip during the operation.
-                            Expects a tuple of (src_top, src_left).
-
-        :rtype:             ``VideoNode``
-        """
-        return core.resize.Bicubic(
-            clip, width, height, src_top=shift[0], src_left=shift[1],
-            filter_param_a=self.b, filter_param_b=self.c, **self.kwargs
-        )
-
-    def descale(self, clip: vs.VideoNode, width: int, height: int, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
-        """
-        Perform a regular descaling operation.
-
-        :param clip:        Input clip
-        :param width:       Output width
-        :param height:      Output height
-        :param shift:       Shift clip during the operation.
-                            Expects a tuple of (src_top, src_left).
-
-        :rtype:             ``VideoNode``
-        """
-        return core.descale.Debicubic(
-            clip, width, height, b=self.b, c=self.c, src_top=shift[0], src_left=shift[1]
-        )
-
-    def resample(
-        self, clip: vs.VideoNode, format: VideoFormatT, matrix: MatrixT = None, matrix_in: MatrixT = None
-    ) -> vs.VideoNode:
-        """
-        Perform a regular resampling operation.
-
-        :param clip:        Input clip
-        :param format:      Output format
-        :param matrix:      Output matrix. If `None`, will take the matrix from the input clip's frameprops.
-        :param matrix_in:   Input matrix. If `None`, will take the matrix from the input clip's frameprops.
-
-        :rtype:             ``VideoNode``
-        """
-        return core.resize.Bicubic(
-            clip, format=int(format),
-            filter_param_a=self.b, filter_param_b=self.c,
-            matrix=matrix, matrix_in=matrix_in, **self.kwargs
-        )
-
-    def shift(self, clip: vs.VideoNode, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
-        """
-        Perform a regular shifting operation.
-
-        :param clip:        Input clip
-        :param shift:       Shift clip during the operation.
-                            Expects a tuple of (src_top, src_left).
-
-        :rtype:             ``VideoNode``
-        """
-        return core.resize.Bicubic(
-            clip, src_top=shift[0], src_left=shift[1],
-            filter_param_a=self.b, filter_param_b=self.c,
-            **self.kwargs
-        )
-
-
 class BSpline(Bicubic):
     """Bicubic b=1, c=0"""
 
@@ -753,6 +676,83 @@ class Bohman(Impulse):
             -0.00264747, -0.001192, 0, 0.000798, 0.00118257, 0.001221, 0.00102559, 0.00072, 0.000408479,
             0.000159, 0, -0.000073, -0.0000834291, -0.000062, -0.000033957, -0.000013, -0.00000291302, 0
         ], 8, **kwargs)
+
+
+class Example(Kernel):
+    """Example Kernel class for documentation purposes."""
+
+    def __init__(self, b: float = 0, c: float = 1/2, **kwargs: Any) -> None:
+        self.b = b
+        self.c = c
+        super().__init__(**kwargs)
+
+    def scale(self, clip: vs.VideoNode, width: int, height: int, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular scaling operation.
+
+        :param clip:        Input clip
+        :param width:       Output width
+        :param height:      Output height
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+
+        :rtype:             ``VideoNode``
+        """
+        return core.resize.Bicubic(
+            clip, width, height, src_top=shift[0], src_left=shift[1],
+            filter_param_a=self.b, filter_param_b=self.c, **self.kwargs
+        )
+
+    def descale(self, clip: vs.VideoNode, width: int, height: int, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular descaling operation.
+
+        :param clip:        Input clip
+        :param width:       Output width
+        :param height:      Output height
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+
+        :rtype:             ``VideoNode``
+        """
+        return core.descale.Debicubic(
+            clip, width, height, b=self.b, c=self.c, src_top=shift[0], src_left=shift[1]
+        )
+
+    def resample(
+        self, clip: vs.VideoNode, format: VideoFormatT, matrix: MatrixT = None, matrix_in: MatrixT = None
+    ) -> vs.VideoNode:
+        """
+        Perform a regular resampling operation.
+
+        :param clip:        Input clip
+        :param format:      Output format
+        :param matrix:      Output matrix. If `None`, will take the matrix from the input clip's frameprops.
+        :param matrix_in:   Input matrix. If `None`, will take the matrix from the input clip's frameprops.
+
+        :rtype:             ``VideoNode``
+        """
+        return core.resize.Bicubic(
+            clip, format=int(format),
+            filter_param_a=self.b, filter_param_b=self.c,
+            matrix=matrix, matrix_in=matrix_in, **self.kwargs
+        )
+
+    def shift(self, clip: vs.VideoNode, shift: Tuple[float, float] = (0, 0)) -> vs.VideoNode:
+        """
+        Perform a regular shifting operation.
+
+        :param clip:        Input clip
+        :param shift:       Shift clip during the operation.
+                            Expects a tuple of (src_top, src_left).
+
+        :rtype:             ``VideoNode``
+        """
+        return core.resize.Bicubic(
+            clip, src_top=shift[0], src_left=shift[1],
+            filter_param_a=self.b, filter_param_b=self.c,
+            **self.kwargs
+        )
 
 
 excluded_kernels = [Kernel, FmtConv, Example]
