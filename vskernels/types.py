3fc1a73c29d7ce287725017239a918a891ba598c
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-24 15:37:40 +0200

    Update Matrix enum

--- vskernels/types.py
+++ vskernels/types.py
@@ -1,11 +1,26 @@
+from __future__ import annotations
+
 from enum import IntEnum
-from typing import NoReturn, Union
+from typing import TYPE_CHECKING, Any, NoReturn, Type, Union
 
 import vapoursynth as vs
 
 
 class Matrix(IntEnum):
     """Matrix coefficients (ITU-T H.265 Table E.5)."""
+
+    _value_: int
+
+    @classmethod
+    def _missing_(cls: Type[Matrix], value: Any) -> Matrix | None:
+        if value is None:
+            return Matrix.UNKNOWN
+
+        return None
+
+    if TYPE_CHECKING:
+        def __new__(cls: type[Matrix], value: int | Matrix | vs.MatrixCoefficients | None) -> Matrix:
+            ...
 
     RGB = 0
     GBR = 0
