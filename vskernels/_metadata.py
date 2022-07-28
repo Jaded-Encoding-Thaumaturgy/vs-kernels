db709b683773d8e714fd723f872d137b1d132a76
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-28 18:36:13 +0200

    Bump up version

--- vskernels/_metadata.py
+++ vskernels/_metadata.py
@@ -1,6 +1,6 @@
 """Kernel objects for scaling and format conversion within VapourSynth"""
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 __author__ = 'LightArrowsEXE <LightArrowsReboot@gmail.com>'
 __maintainer__ = __author__
