af779a064c6fb3c738bb23b0a2a79849becf0d8c
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-14 16:13:32 +0200

    Remove duplicated kernels

--- vskernels/kernels/various.py
+++ vskernels/kernels/various.py
@@ -4,52 +4,10 @@ from typing import Any
 
 import vapoursynth as vs
 
-from .abstract import Kernel
 from .fmtconv import FmtConv
 from .placebo import Placebo
 
 core = vs.core
-
-
-class Point(Kernel):
-    """Built-in point resizer."""
-
-    scale_function = core.resize.Point
-    descale_function = core.resize.Point
-
-
-class Bilinear(Kernel):
-    """Built-in bilinear resizer."""
-
-    scale_function = core.resize.Bilinear
-    descale_function = core.descale.Debilinear
-
-
-class Lanczos(Kernel):
-    """
-    Built-in lanczos resizer.
-
-    Dependencies:
-
-    * VapourSynth-descale
-
-    :param taps: taps param for lanczos kernel
-    """
-
-    scale_function = core.resize.Lanczos
-    descale_function = core.descale.Delanczos
-
-    def __init__(self, taps: int = 3, **kwargs: Any) -> None:
-        self.taps = taps
-        super().__init__(**kwargs)
-
-    def get_params_args(
-        self, is_descale: bool, clip: vs.VideoNode, width: int | None = None, height: int | None = None, **kwargs: Any
-    ) -> dict[str, Any]:
-        args = super().get_params_args(is_descale, clip, width, height, **kwargs)
-        if is_descale:
-            return args | dict(taps=self.taps)
-        return args | dict(filter_param_a=self.taps)
 
 
 class Box(FmtConv):
