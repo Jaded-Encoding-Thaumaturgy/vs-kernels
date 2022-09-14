5cf4efc28ef110196a6369b6ed4b7c50bc01c0d6
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-14 20:19:45 +0200

    Simplify kernels.__all__

--- vskernels/kernels/__init__.py
+++ vskernels/kernels/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from itertools import chain
-
 from . import docs  # noqa: F401
 from .abstract import *  # noqa: F401, F403
 from .bicubic import *  # noqa: F401, F403
@@ -11,8 +9,3 @@ from .placebo import *  # noqa: F401, F403
 from .resize import *  # noqa: F401, F403
 from .spline import *  # noqa: F401, F403
 from .various import *  # noqa: F401, F403
-
-_modules = ['abstract', 'bicubic', 'fmtconv', 'impulse', 'placebo', 'resize', 'spline', 'various']
-__all__ = list(chain.from_iterable(
-    __import__(__name__ + '.' + _module, fromlist=_modules).__all__ for _module in _modules
-))
