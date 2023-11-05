7c64c454a25989774c341c01f00bc8911014a839
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-11-05 15:57:52 +0100

    Modify Gaussian's curve to take the standard sigma over fmtc's scaling

--- vskernels/kernels/various.py
+++ vskernels/kernels/various.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from math import log, sqrt
 from typing import Any
+
+from vstools import CustomValueError, to_singleton
 
 from .fmtconv import FmtConv
 from .placebo import Placebo
@@ -51,8 +54,50 @@ class Gaussian(FmtConv):
 
     _kernel = 'gaussian'
 
-    def __init__(self, curve: float = 30, taps: int = 2, **kwargs: Any) -> None:
-        super().__init__(taps, a1=curve, **kwargs)
+    def __init__(self, sigma: float = 0.5, taps: int = 2, **kwargs: Any) -> None:
+        """
+        Sigma is imagemagick's sigma scaling.
+        This will internally be scaled to fmtc's curve.
+
+        You can specify "curve" to override sigma and specify the original `a1` value.
+        """
+        if 'curve' in kwargs:
+            a1 = kwargs.pop('curve')
+
+            if a1 is not None:
+                if a1 < 1.0 or a1 > 100.0:
+                    raise CustomValueError("curve must be in range 1-100! (inclusive)")
+        else:
+            a1 = self.sigma.to_fmtc(sigma)
+
+            low, up = self.sigma.from_fmtc(100), self.sigma.from_fmtc(1)
+
+            if a1 < 1.0 or a1 > 100.0:
+                raise CustomValueError(f"sigma must be in range {low:.4f}-{up:.4f}! (inclusive)")
+
+        super().__init__(taps, a1=a1, **kwargs)
+
+    @to_singleton
+    class sigma:
+        def from_fmtc(self, curve: float) -> float:
+            if not curve:
+                return 0.0
+            return sqrt(1.0 / (2.0 * (curve / 10.0) * log(2)))
+
+        def to_fmtc(self, sigma: float) -> float:
+            if not sigma:
+                return 0.0
+            return 10 / (2 * log(2) * (sigma ** 2))
+
+        def from_libplacebo(self, sigma: float) -> float:
+            if not sigma:
+                return 0.0
+            return sqrt(sigma / 4)
+
+        def to_libplacebo(self, sigma: float) -> float:
+            if not sigma:
+                return 0.0
+            return 4 * (sigma ** 2)
 
 
 class EwaBicubic(Placebo):
