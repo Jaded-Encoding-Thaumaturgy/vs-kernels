aa68f224afb15949faf77e3f7b98cdbe11fc1e43
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-10-10 20:01:19 +0200

    Bump version

--- vskernels/_metadata.py
+++ vskernels/_metadata.py
@@ -1,6 +1,6 @@
 """Kernel objects for scaling and format conversion within VapourSynth"""
 
-__version__ = '3.2.0'
+__version__ = '3.3.0'
 
 __author_name__, __author_email__ = 'LightArrowsEXE', 'LightArrowsReboot@gmail.com'
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
