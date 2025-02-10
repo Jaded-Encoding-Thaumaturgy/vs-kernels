9cf2baba4b80df825519a9541dd9fb8df10e09dc
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-02-10 02:26:44 +0100

    bump minor version

--- vskernels/_metadata.py
+++ vskernels/_metadata.py
@@ -1,6 +1,6 @@
 """Kernel objects for scaling and format conversion within VapourSynth"""
 
-__version__ = '3.4.0'
+__version__ = '3.4.1'
 
 __author_name__, __author_email__ = 'LightArrowsEXE', 'LightArrowsReboot@gmail.com'
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
