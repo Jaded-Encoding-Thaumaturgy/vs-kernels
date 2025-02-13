6a5c99417408a18ce7b253dac638ababc2d9fc81
arch1t3cht <arch1t3cht@gmail.com>
2025-02-13 15:30:01 +0100

    Final version bump to show deprecation warning

--- vskernels/_metadata.py
+++ vskernels/_metadata.py
@@ -1,6 +1,6 @@
 """Kernel objects for scaling and format conversion within VapourSynth"""
 
-__version__ = '3.4.2'
+__version__ = '3.4.3'
 
 __author_name__, __author_email__ = 'LightArrowsEXE', 'LightArrowsReboot@gmail.com'
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
